cfeca16de140ec8ce9659f67dfc3e0e8c47a90a0
marlonlewis2020 <marlonlewis483@gmail.com>
2023-03-12 19:20:55 -0500

    Added the templates, database models and forms

--- app/views.py
+++ app/views.py
@@ -13,6 +13,9 @@ from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
 from app.models import User, Property
 from app.forms import PropertyForm
+from flask_paginate import Pagination
+import locale
+locale.setlocale(locale.LC_ALL,"")
 
 
 ###
@@ -28,7 +31,84 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Marlon Lewis")
+
+
+@app.route("/properties/<int:propertyid>", methods=["GET"])
+def details(propertyid):
+    """Render the website's about page."""
+    unit = Property.query.filter_by(id=propertyid).first()
+    return render_template('details.html', property=unit)
+
+@app.route("/properties/create", methods=["POST", "GET"])
+def add_property():
+    """Render the website's page to add a new property."""
+    html = "add_property.html"
+    form = PropertyForm()
+    if form.validate_on_submit():
+        # form.validate()
+        if len(form.errors) > 0:
+            for k,v in form.errors.items():
+                print(k,v)
+            new_form = PropertyForm()
+            flash_errors(form)
+            return render_template(html, property = new_form, errors = form.errors.keys())
+        
+        form.filename = form.create_filename()
+        photo = request.files['photo'] or form.photo.data
+        file_path = app.config['UPLOAD_FOLDER']
+        file_name = os.path.join(file_path, form.filename)
+        try:
+            if not os.path.exists(file_path):
+                os.mkdir(file_path)
+            photo.save(file_name)
+            form.save()
+            form = PropertyForm()
+            page = request.args.get('page', 1, type=int)
+            ROWS_PER_PAGE = 6
+            search = False
+            q = request.args.get('q')
+            if q:
+                search = True
+            properties = Property.query.order_by(Property.id.desc()).paginate(page=page, per_page=ROWS_PER_PAGE)
+            total = Property.query.count()
+            pagination = Pagination(page=page, total=total, search=search, record_name='properties')
+            flash("New property added successfully!", 'success')
+            return render_template("properties.html", properties = properties, pagination = pagination, total=total)
+        except FileNotFoundError:
+            if os.path.exists(file_name):
+                os.remove(file_name)
+            flash("Unable to save property image. Try again.", 'warning')
+    return render_template(html, property = form)
+
+@app.route("/properties", methods=["GET"])
+def properties():
+    page = request.args.get('page', 1, type=int)
+    ROWS_PER_PAGE = 6
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+        
+    properties = Property.query.order_by(Property.id.desc()).paginate(page=page, per_page=ROWS_PER_PAGE)
+    total = Property.query.count()
+    pagination = Pagination(page=page, total=total, search=search, record_name='properties')
+    flash("New property added successfully!", 'success')
+    return render_template("properties.html", properties = properties, pagination = pagination, total=total)
+
+
+@app.route("/properties/<filename>", methods=["GET"])
+def get_image(filename):
+    uploads = get_uploaded_images()
+    print("FILENAME:", filename)
+    for file, file_dir in uploads:
+        print("NAME:", filename, "FILE:", file)
+        if filename == file:
+            print(file, file_dir)
+            return send_from_directory(file_dir, file)
+    return page_not_found("Image File Not Found!")
+
+
 
 
 ###
@@ -56,6 +136,14 @@ def send_text_file(file_name):
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
+def get_uploaded_images():
+    photos = []
+    for dir, subdirs, files in os.walk(os.getcwd() + app.config['UPLOAD_FOLDER']):
+        for file in files:
+            photos.append([file, dir])
+            print(file, dir)
+    return photos
+
 
 @app.after_request
 def add_header(response):
@@ -73,3 +161,9 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+def currency_format(value):
+    return locale.currency(float(value), grouping=True)
+
+app.jinja_env.filters['currency_format'] = currency_format
